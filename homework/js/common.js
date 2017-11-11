d5ad6f724f8782f0f579d551c0caed4c122e2139
Illarion K <larick90@gmail.com>
2017-11-11 23:36:42 +0200

    homework

--- homework/js/common.js
+++ homework/js/common.js
@@ -15,4 +15,33 @@ function findX(a, b, c) {
     }
 }
 
-findX(1, -26, 120);
+var m;
+var n;
+var r;
+
+// Меньше число -----------------------------------------------------------
+function findMin(m, n) {
+    if (m < n) {
+        console.log('Меньше число', m);
+    } else if (m > n) {
+        console.log('Меньше число', n);
+    } else console.log('Числа рівні');
+    // return Math.min(m, n);
+}
+
+// Підняття до степені -----------------------------------------------------------
+function findPow(m, n) {
+    if ( n !== undefined ) {
+        r = Math.pow(m, n);
+        console.log('Число "m" в степені "n" = ', r);
+    } else {
+        r = m * m;
+        console.log('Число "m" в степені "m" = ', r);
+    }
+}
+
+// Середнє арифметичне -----------------------------------------------------------
+function findMid(m, n) {
+    r = (m + n) / 2;
+    console.log('----', 'Середнє арифметичне =', r);
+}
