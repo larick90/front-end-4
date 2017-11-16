6e3f80c3b5c84518d9e811105863f83d99e0624f
Illarion K <larick90@gmail.com>
2017-11-17 00:02:31 +0200

    homework

---
+++ homework/js/common.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// 1.1 Функція для визначення чи об'єкт пустий
+
+var arr = [ 45, 109, 32, 39, 1, 0 ];
+
+function isEmpty(arr) {
+    for ( var key in arr ) {
+        return true;
+    }
+    return false;
+}
+
+// 1.2 Сума об'єкту
+
+function sumData(arr) {
+    if (isEmpty(arr)) {
+        var sumArr = 0;
+        for ( var key in arr ) {
+            sumArr += arr[key];
+        } return sumArr;
+    } else {
+        console.log('----', 'масив порожній');
+    }
+}
+
+// 2 Об'єкт з масивом і його сума
+
+var codeObj = [ 45, 109, 32, 39, 1, 0 ];
+
+codeObj[23] = 95;
+
+function sumObj(codeObj) {
+    var sum = 0;
+    for ( var key in codeObj ) {
+        sum += codeObj[key];
+    } return sum;
+}
