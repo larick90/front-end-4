1f29b8d5fd47a90053cc523cf4c9a6b65066a7e7
Illarion K <larick90@gmail.com>
2017-12-15 18:41:20 +0200

    submenu

--- EnvyMe/script/common.js
+++ EnvyMe/script/common.js
@@ -1,10 +1,15 @@
 "use strict";
 
 $(function(){
+    //search
+    $('header-search').on("click", function () {
+
+    });
+
+    //sliders
     $('.slider').slick({
         autoplay: true,
-        arrows: false,
-        dots: true
+        arrows: false
     });
     $('.rec-list').slick({
         infinite: true,
